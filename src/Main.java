e07338813e5c13802d0e3a6a0249ce141adf21de
Sergey <enotagressor@gmail.com>
2022-04-25 18:24:04 +0700

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("nihao");
+        task3();
+    }
+
+    public static void task1() {
+        int i = 1;
+        int j = 10;
+        int k = i;
+        while (i <= j) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.print("\n");
+            for (i=i-1; i >= k; i--) {
+                System.out.print(i + " ");
+            }
+        }
+
+    public static void task2(){
+        int f = 6;
+        int d = 31;
+        for (; f <= d; f = f + 7){
+            System.out.println("Сегодня пятница " + f + " число. Необходимо подготовить отчет!");
+        }
+    }
+    public static void task3() {
+        int y = 2022;
+        int n = 200;
+        int e = 100;
+        int o = 0;
+        int k = 79;
+
+        for (; o <= (y + e); o = o + k){
+            if (o >= (y - n)){
+                System.out.println(o);
+            }
+        }
+
     }
 }
+
